5241be41ab1fee161a11e96b619700067dff897d
VliegendeLeeuw <63657291+VliegendeLeeuw@users.noreply.github.com>
2020-11-05 21:13:49 +0100

    Change

--- countdown.js
+++ countdown.js
@@ -9,8 +9,10 @@ var myfunc = setInterval(function() {
     var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
-    document.getElementById("days").innerHTML = days
-    document.getElementById("hours").innerHTML = hours
-    document.getElementById("mins").innerHTML = minutes
-    document.getElementById("secs").innerHTML = seconds
+    document.querySelector("#countdown").innerHTML = `Het duurt nog ${days} dagen, ${hours} uur, ${minutes} minuten en ${seconds} seconden tot 1 december!`
+
+  //  document.getElementById("days").innerHTML = days
+  //  document.getElementById("hours").innerHTML = hours
+  //  document.getElementById("mins").innerHTML = minutes
+  //  document.getElementById("secs").innerHTML = seconds
 }, 1000)
