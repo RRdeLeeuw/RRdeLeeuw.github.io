517cd55770032cd9cb0cf8ed7ddb8bc9fa43533a
VliegendeLeeuw <63657291+VliegendeLeeuw@users.noreply.github.com>
2020-11-05 16:50:15 +0100

    Removed JS to seperate file.

---
+++ countdown.js
@@ -0,0 +1,16 @@
+var countDownDate = new Date("2020-12-01T00:00:00+01:00");
+
+var myfunc = setInterval(function() {
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+        
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days
+    document.getElementById("hours").innerHTML = hours
+    document.getElementById("mins").innerHTML = minutes
+    document.getElementById("secs").innerHTML = seconds
+}, 1000)
